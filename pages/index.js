5bb1d533e5e07d837bb9e24e82ba753bed0e9796
seotrot <usmaniseo@gmail.com>
2021-08-26 15:20:07 +0500

    api-url

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
   )
   const submitHandler = async (e) => {
     e.preventDefault()
-    const response = await fetch(`http://localhost:3000/api/urls`, {
+    const response = await fetch(`/api/urls`, {
       method: 'POST',
       body: JSON.stringify({ keyword }),
       headers: {
